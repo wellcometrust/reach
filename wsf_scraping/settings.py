ca8cf74130653f576d9f2147361fa29c2f115eab
Depardieu <s.depardieu@wellcome.ac.uk>
2017-12-15 10:44:26 +0000

    Settings tuning, including log modification, scraped years, change on feed configuration and comments

--- wsf_scraping/settings.py
+++ wsf_scraping/settings.py
@@ -17,17 +17,14 @@ BOT_NAME = 'wsf_scraper'
 SPIDER_MODULES = ['wsf_scraping.spiders']
 NEWSPIDER_MODULE = 'wsf_scraping.spiders'
 
-# LOG_ENABLED = False
+# Logging settings: Log all into a file, basic level is INFO.
 LOG_LEVEL = 'INFO'
-LOF_FILE = 'var/log-{log_level}.txt'.format(log_level=LOG_LEVEL)
-# Set pdfminer log to WARNING
+LOG_FILE = 'var/log-{log_level}.txt'.format(log_level=LOG_LEVEL)
+LOG_SDTOUT = True
+LOG_ENABLED = True
+# Set pdfminer log to WARNING, should stay that way as pdfminer echoes a lot
+# of unuseful log.
 logging.getLogger("pdfminer").setLevel(logging.WARNING)
-
-# Force UTF-8 encoding
-FEED_EXPORT_ENCODING = 'utf-8'
-
-# Crawl responsibly by identifying yourself (and your website)
-# USER_AGENT = 'wsf_scraping (+wateystrdjytkfu)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -43,34 +40,33 @@ DOWNLOAD_TIMEOUT = 360
 SCHEDULER_MEMORY_QUEUE = 'scrapy.squeues.FifoMemoryQueue'
 DUPEFILTER_CLASS = "wsf_scraping.filter.BLOOMDupeFilter"
 
-# Autothrottle for best performances
+# Autothrottle for best performances, and to be more respectful of the website
+# we are scraping.
 AUTOTHROTTLE_ENABLED = True
 AUTOTHROTTLE_START_DELAY = 5.0
 AUTOTHROTTLE_MAX_DELAY = 60.0
 AUTOTHROTTLE_TARGET_CONCURRENCY = 2.0
 
-# Disable cookies (enabled by default)
+# Disable cookies
 COOKIES_ENABLED = False
 
-#  who_iris and who_iris_single_page per-page results
+#  who_iris and who_iris_single_page dedicated settings
 WHO_IRIS_RPP = 250
-WHO_IRIS_YEARS = [2012]
+WHO_IRIS_YEARS = [2012, 2013, 2014, 2015, 2016, 2017]
 
 # Jsonlines are cleaner for big feeds
 FEED_FORMAT = 'jsonlines'
 FEED_EXPORT_ENCODING = 'utf-8'
 
 # Prod feed export to amazon s3
-FEED_CONFIG = 'PROD'
+FEED_CONFIG = 'DEBUG'
 
-if FEED_CONFIG == 'DEBUG':
-    FEED_URI = './results/who_{date}.json'.format(date=datetime.datetime.now())
-else:
+if FEED_CONFIG == 'S3':
     AWS_ACCESS_KEY_ID = "______your_aws_id______"
     AWS_SECRET_ACCESS_KEY = "______your_aws_secret______"
-    FEED_URI = 's3://______your_s3_folder_name______/who_{date}.json'.format(
-        date=datetime.datetime.now()
-    )
+    FEED_URI = 's3://__your_s3_bucket__/scraping/feeds/%(name)s/%(time)s.json'
+else:
+    FEED_URI = './results/who_{date}.json'.format(date=datetime.datetime.now())
 
 # Lists to look for (case insensitive)
 SEARCH_FOR_LISTS = ['reference', 'bibliography', 'citation']
