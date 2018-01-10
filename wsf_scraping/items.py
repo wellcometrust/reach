15d67b73d4a375ba93c54d4a6b5ebd7b80f14f33
Depardieu <s.depardieu@wellcome.ac.uk>
2018-01-10 11:13:42 +0000

    Add a pdf file field on items, giving the name of the linked PDF

--- wsf_scraping/items.py
+++ wsf_scraping/items.py
@@ -12,6 +12,7 @@ class WHOArticle(scrapy.Item):
     title = scrapy.Field()
     uri = scrapy.Field()
     year = scrapy.Field()
+    pdf = scrapy.Field()
     authors = scrapy.Field()
     sections = scrapy.Field()
     keywords = scrapy.Field()
