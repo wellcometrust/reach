426607130c3e3005bf59c686a34286adacb4d0fe
Samuel Depardieu <6536364+SamDepardieu@users.noreply.github.com>
2018-06-05 14:30:58 +0100

    Update the entrypoint script to allow empty variables and scraper settings (#75)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-set -o nounset
 set -o errexit
 
-echo "Running $SPIDER_TO_RUN spider."
-scrapy crawl "$SPIDER_TO_RUN"
+if [[ -z "$SCRAPY_YEARS" ]]; then
+  YEARS_LIST=''
+else
+  YEARS_LIST="-a years_list=\"$SCRAPY_YEARS\""
+fi
+if [[ -z "$SCRAPY_OPTIONS" ]]; then
+    OPTIONS_LIST=''
+else
+    OPTIONS_LIST=$SCRAPY_OPTIONS
+fi
+if [[ -z "$SPIDER_TO_RUN" ]]; then
+    echo "Either you did not specify a spider, or the spider you want to run does not exist."
+    exit 1
+fi
+
+echo "Running scrapy crawl $SPIDER_TO_RUN $YEARS_LIST $OPTIONS_LIST"
+scrapy crawl "$SPIDER_TO_RUN" $YEARS_LIST $OPTIONS_LIST
 
 exit 0
