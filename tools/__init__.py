0ba793d7e54fbf17719ba8cc01268a04f28a7135
Samuel Depardieu <6536364+SamDepardieu@users.noreply.github.com>
2018-09-25 15:54:58 +0100

    Remove DynamoDB testing as we're not using dynamo anymore (#94)

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,6 +1,5 @@
 from .dbTools import DatabaseConnector
 from . import utils
 from .AWSFeedStorage import AWSFeedStorage
-from .dynamodbConnector import DynamoDBConnector
 
-__all__ = [DatabaseConnector, utils, AWSFeedStorage, DynamoDBConnector]
+__all__ = [DatabaseConnector, utils, AWSFeedStorage]
