4e77813d42625232b6f2de154e029acf146ef661
SamDepardieu <sam.depardieu@gmail.com>
2018-10-24 15:43:53 +0100

    :art: Add some logging and start a variable name clarification work

--- main.py
+++ main.py
@@ -9,40 +9,49 @@ from settings import settings
 def run_predict(scraper_file, references_file,
                 model_file, vectorizer_file):
     logger = settings.logger
-    mode = 'S3' if not settings.DEBUG else 'LOCAL'
+    logger.setLevel('INFO')
+    mode = 'S3' if settings.S3 else 'LOCAL'
     fm = FileManager(mode)
     predicter = Predicter()
-    logger.info("Reading input files for %s", settings.ORGANISATION)
+    logger.info("[+] Reading input files for %s", settings.ORGANISATION)
 
+    # Loading the scraper results
     scraper_file_name = os.path.basename(scraper_file)
     scraper_file_dir = os.path.dirname(scraper_file)
-    raw_text_data = fm.get_file(scraper_file_name, scraper_file_dir, 'json')
+    scraper_file = fm.get_file(scraper_file_name, scraper_file_dir, 'json')
 
+    # Loading the references file
     ref_file_name = os.path.basename(references_file)
     ref_file_dir = os.path.dirname(references_file)
-    WT_references = fm.get_file(ref_file_name, ref_file_dir, 'csv')
+    ref_file = fm.get_file(ref_file_name, ref_file_dir, 'csv')
 
+    # Loading the model and the vectoriser
     model_file_name = os.path.basename(model_file)
     model_file_dir = os.path.dirname(model_file)
     mnb = fm.get_file(model_file_name, model_file_dir, 'pickle')
 
-    vect_file_name = os.path.basename(model_file)
-    vect_file_dir = os.path.dirname(model_file)
+    vect_file_name = os.path.basename(vectorizer_file)
+    vect_file_dir = os.path.dirname(vectorizer_file)
     vectorizer = fm.get_file(vect_file_name, vect_file_dir, 'pickle')
 
-    reference_components = process_reference_section(
-        raw_text_data,
+    # Split the reference sections using regex
+    logger.info('[+] Spliting the references')
+    splited_references = process_reference_section(
+        scraper_file,
         settings.ORGANISATION_REGEX
     )
 
     t0 = time.time()
 
+    # Predict the references types (eg title/author...)
+    logger.info('[+] Predicting the reference components')
     reference_components_predictions = predicter.predict_references(
         mnb,
         vectorizer,
-        reference_components
+        splited_references
     )
 
+    # Predict the reference structure????
     predicted_reference_structures = predicter.predict_structure(
         reference_components_predictions,
         settings.PREDICTION_PROBABILITY_THRESHOLD
@@ -50,7 +59,7 @@ def run_predict(scraper_file, references_file,
     predicted_reference_structures['Organisation'] = settings.ORGANISATION
 
     fuzzy_matcher = FuzzyMatcher(
-        WT_references,
+        ref_file,
         settings.FUZZYMATCH_THRESHOLD
     )
     all_match_data = fuzzy_matcher.fuzzy_match_blocks(
@@ -70,9 +79,9 @@ def run_predict(scraper_file, references_file,
     total = t1-t0
 
     logger.info(
-        "Time taken to predict and match for ",
-        str(len(reference_components)),
-        " is ", str(total)
+        "Time taken to predict and match for %s is %s",
+        str(len(splited_references)),
+        str(total)
     )
 
 
