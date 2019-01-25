9a1cd9def882af82197e5ce36bdaeeded78dd281
Nick Sorros <nsorros@gmail.com>
2019-01-25 16:28:29 +0000

    Address review comments

--- tests/test_separate.py
+++ tests/test_separate.py
@@ -1,7 +1,7 @@
 import unittest
 
-from utils import split_sections
-
+from utils import split_sections, process_reference_section
+from main import SectionedDocument
 
 class TestSplit(unittest.TestCase):
 	
@@ -21,3 +21,14 @@ class TestSplit(unittest.TestCase):
 		references = split_sections("One reference\nTwo references\nThree references\n")
 		self.assertEqual(references, ["One reference", "Two references", "Three references"],
 			"Should be ['One reference', 'Two reference', 'Three reference']")
+
+class TestProcessReferenceSection(unittest.TestCase):
+
+	def test_no_section_in_document(self):
+		doc = SectionedDocument(
+			None,
+			'uri',
+			'id'
+		)
+		with self.assertRaises(TypeError):
+			process_reference_section(doc, '\n')
