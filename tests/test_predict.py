a1668feb64a280d47301515e001713fbfabb2835
Elizabeth Gallagher <gallaghe@01763-IA-GallaghE.local>
2019-01-29 11:38:55 +0000

    changes to test_predict from hunter and nicks comments

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,32 +1,41 @@
+import os.path
 import unittest
+import pickle
 
 from utils import predict_references
 
-import pickle
+MODELS_DIR = os.path.join( os.path.dirname(__file__), '..', 'reference_parser_models')
 
-pickle_in = open("./reference_parser_models/RefSorter_classifier.pkl","rb")
-mnb = pickle.load(pickle_in)
+with open(os.path.join(MODELS_DIR, "RefSorter_classifier.pkl"), "rb") as f:
+    mnb = pickle.load(f)
 
-pickle_in = open("./reference_parser_models/RefSorter_vectorizer.pkl","rb")
-vectorizer = pickle.load(pickle_in)
+with open(os.path.join(MODELS_DIR, "RefSorter_vectorizer.pkl"), "rb") as f:
+    vectorizer = pickle.load(f)
 
-class TestSplit(unittest.TestCase):
+class TestPredict(unittest.TestCase):
 	def test_empty_components(self):
-		components_predictions = predict_references(mnb,vectorizer,"")
+		components_predictions = predict_references(mnb,vectorizer,[])
 		self.assertEqual(components_predictions, [], "Should be []")
 
 	def test_normal_components(self):
 		components_predictions = predict_references(mnb,vectorizer,["Component","Component","Component"])
-		self.assertEqual(isinstance(components_predictions, list) , True, "Should be a list")
+		all_categories = ['Authors', 'Title', 'Journal', 'PubYear', 'Volume', 'Issue', 'Pagination']
+		self.assertEqual(
+			isinstance(components_predictions, list) and
+			all([components_prediction[0] in all_categories for components_prediction in components_predictions]),
+			True, "Should be a list of categories from {}".format(all_categories)
+			)
 
 	def test_size(self):
 		components_predictions = predict_references(mnb,vectorizer,["Component","Component","Component"])
 		self.assertEqual(len(components_predictions) , 3, "Should be 3")
 
-	def test_author_component(self):
+	def test_string_component(self):
 		components_predictions = predict_references(mnb,vectorizer,["Component"])
 		self.assertEqual(isinstance(components_predictions[0][0], str), True, "Should be a string")
 
 	def test_year_component(self):
 		components_predictions = predict_references(mnb,vectorizer,["1999"])
 		self.assertEqual(components_predictions[0][0], 'PubYear', "Should be 'PubYear'")
+
+
