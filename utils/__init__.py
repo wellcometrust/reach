9f3e7649a8b5845809ad006d9c2727f7f7e6174c
Nick Sorros <nsorros@gmail.com>
2019-02-06 14:40:20 +0000

    Rename variables, functions and file in separate

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,19 +1,16 @@
-from .separate import (process_reference_section,
-                       split_sections)
-from .predict import predict_references, predict_structure, split_reference, process_references
+from .split import process_references_section
+from .predict import predict_references, predict_structure, process_references
 from .fuzzymatch import FuzzyMatcher
 from .file_manager import FileManager
 from .serialiser import serialise_matched_reference, serialise_reference
 
 __all__ = [
-    process_reference_section,
+    process_references_section,
     process_references,
     FuzzyMatcher,
     FileManager,
     serialise_matched_reference,
     serialise_reference,
-    split_sections,
-    split_reference,
     predict_references,
     predict_structure
 ]
