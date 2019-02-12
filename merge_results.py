daa6b8acc50c9684a5084a78705d312395cc9f30
Elizabeth Gallagher <lizgzil@hotmail.com>
2019-02-12 10:30:40 +0000

    writing it so that the job name matched results are automatically found for each organisation, rather than having to input each of the folder names for each org

--- merge_results.py
+++ merge_results.py
@@ -4,6 +4,7 @@ Manually edit main with the folder names you want to merge all together
 Output is one csv of all the matches from all the organisations you have provided
 """
 
+import fnmatch
 import os
 import pandas as pd
 
@@ -23,16 +24,17 @@ def get_matches(all_matches, all_uri, file_dir):
 
     return all_matches, all_uri
 
-def merge_matches(folder_name, org_list, match_refs):
+def merge_matches(folder_dir, job_name, match_refs):
     """
     Get all the _all_match_data.csv files for all organisations
     Concat them all and join to the original references data
     """
     all_matches = []
     all_uri = []
-    for org in org_list:
-        file_dir = "{}/{}".format(folder_name, org)
-        all_matches, all_uri = get_matches(all_matches, all_uri, file_dir)
+    for folder_name in fnmatch.filter(os.listdir(folder_dir), '{}_*'.format(job_name)):
+        if os.path.isdir(folder_dir+folder_name):
+            file_dir = folder_dir+folder_name
+            all_matches, all_uri = get_matches(all_matches, all_uri, file_dir)
 
     all_matches = pd.concat(all_matches, ignore_index=False)
     all_uri = pd.concat(all_uri, ignore_index=False)
@@ -52,19 +54,17 @@ def merge_matches(folder_name, org_list, match_refs):
 if __name__ == '__main__':
 
     # The name you used in your output file name
-    job_name = "charlene" # "pophealth"
+    job_name = "charlene" # "pophealth", "charlene"
     
     # The original references data searched for
     match_refs = pd.read_csv(
         "/Users/gallaghe/Code/reference-parser/match-references/charlene_publications_format.csv")
     # "/Users/gallaghe/Code/reference-parser/match-references/MRC_Publications_Nov2018_JGHT_JHSRI_all.csv"
+    # "/Users/gallaghe/Code/reference-parser/match-references/charlene_publications_format.csv"
 
-    folder_name = "/Users/gallaghe/Code/reference-parser/tmp/parser-output"
-    who_iris = "{}_who_iris20190207".format(job_name)
-    unicef = "{}_unicef20190131".format(job_name)
-    msf = "{}_msf20190131".format(job_name)
-    nice = "{}_nice20190131".format(job_name)
+    folder_dir = './tmp/parser-output/'
 
-    all_matches_refs = merge_matches(folder_name, [who_iris, unicef, msf, nice], match_refs)
+    all_matches_refs = merge_matches(folder_dir, job_name, match_refs)
 
-    all_matches_refs.to_csv('{}/{}_all_matches.csv'.format(folder_name, job_name))
+    all_matches_refs.to_csv('{}/{}_all_matches.csv'.format(folder_dir, job_name))
+
