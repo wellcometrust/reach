1e2ffb0507cc2e8b91fa68f9b25fdccb93c0a575
Elizabeth Gallagher <lizgzil@hotmail.com>
2019-02-14 12:08:07 +0000

    adding arguments to main and fixing some other of the issues in the PR comments

--- merge_results.py
+++ merge_results.py
@@ -4,6 +4,7 @@ Manually edit main with the folder names you want to merge all together
 Output is one csv of all the matches from all the organisations you have provided
 """
 
+from argparse import ArgumentParser
 import fnmatch
 import os
 import glob
@@ -27,7 +28,7 @@ def get_csv_names(file_dir, suffix):
 
     return csv_names
 
-def merge_match_csvs(match_csv_names):
+def concat_match_csvs(match_csv_names):
     """
     This function merges all the csvs given in match_csv_names
     Input:
@@ -41,12 +42,12 @@ def merge_match_csvs(match_csv_names):
         if not match_data.empty:
             all_matches.append(match_data)
 
-    all_matches = pd.concat(all_matches, ignore_index=False)
+    all_matches = pd.concat(all_matches)
 
     return all_matches
 
 
-def merge_predicted_csvs(predicted_csv_names):
+def concat_predicted_csvs(predicted_csv_names):
     """
     This function gets all the document id & document url from the predicted references csv
     Input:
@@ -70,21 +71,35 @@ def merge_predicted_csvs(predicted_csv_names):
 
     return all_url
 
+def create_argparser(description):
+    parser = ArgumentParser(description)
+    parser.add_argument(
+        '--file_dir',
+        help='Directory of saved output folders',
+        default = './tmp/parser-output/charlene'
+    )
+    parser.add_argument(
+        '--match_refs_file',
+        help='Original references file which was used in parsing of saved outputs',
+        default = './match-references/charlene_publications_format.csv'
+    )
+    return parser
+
 if __name__ == '__main__':
 
-    file_dir = './tmp/parser-output/charlene' # "pophealth", "charlene"
-    output_name = 'merged_all_matches'
+    parser = create_argparser(__doc__.strip())
+    args = parser.parse_args()
+
+    file_dir = args.file_dir
+    output_folder_name = 'merged_all_matches'
     
-    match_refs = pd.read_csv(
-        "./match-references/charlene_publications_format.csv")
-    # "./match-references/MRC_Publications_Nov2018_JGHT_JHSRI_all.csv"
-    # "./match-references/charlene_publications_format.csv"
+    match_refs = pd.read_csv(args.match_refs_file)
 
     match_csv_names = get_csv_names(file_dir, '_all_match_data')
     predicted_csv_names = get_csv_names(file_dir, '_predicted_reference_structures')
 
-    all_match = merge_match_csvs(match_csv_names)
-    all_url = merge_predicted_csvs(predicted_csv_names)
+    all_match = concat_match_csvs(match_csv_names)
+    all_url = concat_predicted_csvs(predicted_csv_names)
 
     all_matches_url = all_match.join(
         all_url.set_index('Document id'),
@@ -96,9 +111,9 @@ if __name__ == '__main__':
         on='WT_Ref_Id'
         ) # Join with references information
 
-    if not os.path.exists('{}/{}'.format(file_dir, output_name)):
-            os.makedirs('{}/{}'.format(file_dir, output_name))
+    if not os.path.exists('{}/{}'.format(file_dir, output_folder_name)):
+            os.makedirs('{}/{}'.format(file_dir, output_folder_name))
 
-    all_matches_refs.to_csv('{}/{}/merged_all_matches.csv'.format(file_dir, output_name))
+    all_matches_refs.to_csv('{}/{}/merged_all_matches.csv'.format(file_dir, output_folder_name))
 
 
