eee52d1479249a9bb7f43a27e980b71e9e794a68
SamDepardieu <sam.depardieu@gmail.com>
2019-03-01 14:41:24 +0000

    Change item to remove pdf parsing related fields

--- policytool/scraper/wsf_scraping/items.py
+++ policytool/scraper/wsf_scraping/items.py
@@ -7,20 +7,16 @@ class Article(scrapy.Item):
         return repr({
             'title': self.get('title'),
             'uri': self.get('uri'),
-            'provider': self.get('provider'),
         })
 
     title = scrapy.Field()
     year = scrapy.Field()
     uri = scrapy.Field()
     pdf = scrapy.Field()
-    sections = scrapy.Field()
-    keywords = scrapy.Field()
     hash = scrapy.Field()
     has_text = scrapy.Field()
     types = scrapy.Field()
     subjects = scrapy.Field()
     authors = scrapy.Field()
     types = scrapy.Field()
-    provider = scrapy.Field()
     date_scraped = scrapy.Field()
