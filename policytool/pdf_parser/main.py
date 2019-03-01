30481b46be952817bae66bbe7939b68af6dc3706
SamDepardieu <sam.depardieu@gmail.com>
2019-03-01 13:39:57 +0000

    Add pdf parsing related scrpits to the pdf parser module
     * Adds db tools
     * Adds keyword parsing method
     * Change the main a bit to take these changes into account

--- policytool/pdf_parser/main.py
+++ policytool/pdf_parser/main.py
@@ -8,6 +8,32 @@ logger = logging.getLogger(__name__)
 logger.setLevel('INFO')
 
 
+KEYWORDS_FILE = ''
+SECTIONS_KEYWORDS_FILE = ''
+KEYWORDS_CONTEXT = ''
+
+
+def parse_keywords_files(file_path):
+    """Convert keyword files into lists, ignoring # commented lines."""
+    logger = logging.getLogger(__name__)
+    logger.debug("Try to open keyword files")
+    keywords_list = []
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            logger.debug("Successfully opened %s", file_path)
+            for line in f:
+                line = line.replace('\n', '')
+                if line and line[0] != '#':
+                    keywords_list.append(line.lower())
+    except IOError:
+        logger.warning(
+            "Unable to open keywords file at location %s", file_path
+        )
+        keywords_list = []
+    finally:
+        return keywords_list
+
+
 def check_keywords(self, item, spider_name, base_pdf_path):
     """Convert the pdf file to a python object and analyse it to find
     keywords and section based on the section/keywords files provided.
@@ -22,7 +48,13 @@ def check_keywords(self, item, spider_name, base_pdf_path):
         )
 
         pdf_file = parse_pdf_document(f)
-
+        # Get the sections and keywords to look for.
+        keywords = parse_keywords_files(
+            KEYWORDS_FILE
+        )
+        section_keywords = parse_keywords_files(
+            SECTIONS_KEYWORDS_FILE
+        )
         # If the PDF couldn't be converted, still remove the pdf file
         if not pdf_file:
             os.remove(item['pdf'])
@@ -40,7 +72,7 @@ def check_keywords(self, item, spider_name, base_pdf_path):
         # Fetch references or other keyworded list
         keyword_dict = pdf_file.get_lines_by_keywords(
             keywords,
-            settings['KEYWORDS_CONTEXT']
+            KEYWORDS_CONTEXT
         )
 
         # Add references and PDF name to JSON returned file
