169233e0a81c0fffb5c12d9e95b10c9dbe4606d8
SamDepardieu <sam.depardieu@gmail.com>
2019-03-06 14:13:28 +0000

    Change the DAG name and fix a typo

--- policytool/scraper/airflow/dags/policytool_dag.py
+++ policytool/scraper/airflow/dags/policytool_dag.py
@@ -7,7 +7,7 @@ import datetime
 from airflow import DAG
 import airflow.utils.dates
 
-from scraper.airflow.tasks.dummy_spiders_operator import RunSpidersOperator
+from scraper.airflow.tasks.run_spiders_operator import RunSpiderOperator
 
 
 ORGANISATIONS = [
@@ -27,14 +27,14 @@ args = {
 }
 
 dag = DAG(
-    dag_id='test_dag',
+    dag_id='policytool_dag',
     default_args=args,
     schedule_interval='0 0 * * 0'
 )
 
 
 for organisation in ORGANISATIONS:
-    run_spider = RunSpidersOperator(
+    run_spider = RunSpiderOperator(
         task_id='run_{spider}_spider'.format(spider=organisation),
         organisation=organisation,
         dag=dag,
