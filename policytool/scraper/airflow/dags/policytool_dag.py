33c625c3bf9f9c1ca7d664ad577eb3379241fb4e
SamDepardieu <sam.depardieu@gmail.com>
2019-03-06 18:26:20 +0000

    Changes and features for the policytool dag:
     * Add the pdf parsing task to the tasks and to the dag
     * Create the path for input and output in the dag

--- policytool/scraper/airflow/dags/policytool_dag.py
+++ policytool/scraper/airflow/dags/policytool_dag.py
@@ -3,20 +3,20 @@ DAG to run the policy tool on every organisation.
 """
 
 import datetime
-
+import os
 from airflow import DAG
 import airflow.utils.dates
 
 from scraper.airflow.tasks.run_spiders_operator import RunSpiderOperator
-
+from scraper.airflow.tasks.parse_pdf_operator import ParsePdfOperator
 
 ORGANISATIONS = [
     'who_iris',
     'nice',
     'gov_uk',
-    'msf',
     'unicef',
     'parliament',
+    'msf',
 ]
 
 args = {
@@ -32,10 +32,37 @@ dag = DAG(
     schedule_interval='0 0 * * 0'
 )
 
-
 for organisation in ORGANISATIONS:
+    scraping_path = os.path.join(
+        'datalabs-data',
+        'airflow',
+        'output',
+        'policytool-scrape',
+        'scraper-{organisation}'.format(
+            organisation=organisation
+        ),
+    )
     run_spider = RunSpiderOperator(
         task_id='run_{spider}_spider'.format(spider=organisation),
         organisation=organisation,
+        path=scraping_path,
         dag=dag,
     )
+
+    parsing_path = os.path.join(
+        'datalabs-data',
+        'airflow',
+        'output',
+        'policytool-parse',
+        'parser-{organisation}'.format(
+            organisation=organisation
+        ),
+    )
+    pdfParsing = ParsePdfOperator(
+        task_id='run_{organisation}_parser'.format(organisation=organisation),
+        organisation=organisation,
+        input_path=scraping_path,
+        output_path=parsing_path,
+        dag=dag,
+    )
+    pdfParsing.set_upstream(run_spider)
