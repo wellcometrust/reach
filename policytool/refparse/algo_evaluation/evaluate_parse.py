f14ee0915c816d00dd6f4e0ae0e20f284cdc2c71
Elizabeth Gallagher <lizgzil@hotmail.com>
2019-05-15 13:39:26 +0100

    Add new parser evaluation metric, and add the levenshtein distance threshold parameter to settings

--- policytool/refparse/algo_evaluation/evaluate_parse.py
+++ policytool/refparse/algo_evaluation/evaluate_parse.py
@@ -1,34 +1,70 @@
 
+import pandas as pd
+import numpy as np
+import editdistance
+
 from utils.parse import structure_reference
+from sklearn.metrics.pairwise import cosine_similarity
 
-def evaluate_metric(actual, predicted):
+def get_levenshtein_df(df1, df2):
+    """
+    Input: two equally sized dataframes
+    output: a dataframe of the levenshtein distances between each element pair
+    """
 
-    test_scores = {'Score' : 50, 'More info' : "More information about this test"}
+    calc_lev_dist_column = lambda s1, s2: editdistance.eval(s1, s2)/max(len(s1), len(s2)) if (s1!='' or s2 !='') else 0
+    lev_dist = pd.DataFrame([df1[column].combine(df2[column], calc_lev_dist_column) for column in df1.columns]).T
+
+    return lev_dist
+
+def evaluate_metric(actual_components_list, predicted_components_list, levenshtein_threshold):
+
+    # For the computations below it's useful for any nan's to be blank strings
+    df1 = pd.DataFrame(actual_components_list).replace(np.nan,'').astype(str)
+    df2 = pd.DataFrame(predicted_components_list).replace(np.nan,'').astype(str)
+
+    df1['PubYear'] = [d[0:4] if d!='' else d for d in df1['PubYear']]
+
+    equal = df1 == df2
+
+    proportion_equal_cat = equal.sum()/len(equal)
+    proportion_equal = (equal.sum().sum())/(equal.shape[0]*equal.shape[1])
+
+    lev_dist = get_levenshtein_df(df1, df2)
+
+    quite_equal = lev_dist<levenshtein_threshold
+    proportion_quite_equal_cat = quite_equal.sum()/len(quite_equal)
+    proportion_quite_equal = (quite_equal.sum().sum())/(quite_equal.shape[0]*quite_equal.shape[1])
+
+    test_scores = {
+            'Score' : proportion_equal,
+            'Proportion of components predicted correctly' : proportion_equal,
+            'Proportion of components predicted correctly by category' : proportion_equal_cat,
+            'Mean Levenshtein distance' : lev_dist.mean().mean(),
+            'Mean Levenshtein distance by category' : lev_dist.mean(),
+            'Proportion of components predicted almost correctly (Levenshtein < {})'.format(levenshtein_threshold) : proportion_quite_equal,
+            'Proportion of components predicted almost correctly (Levenshtein < {}) by category'.format(levenshtein_threshold) : proportion_quite_equal_cat
+            }
 
     return test_scores
 
-def evaluate_parse(parse_test_data, model):
+def evaluate_parse(parse_test_data, model, levenshtein_threshold):
 
     predicted_structure = []
     for reference in parse_test_data['Actual reference']:
-
         structured_reference = structure_reference(model, reference)
-
         predicted_structure.append(structured_reference)
 
     parse_test_data['Predicted merged components'] = predicted_structure
 
-    merged_components = {
-    'Authors': parse_test_data['Authors'],
-    'Journal': parse_test_data['Journal'],
-    'Volume': parse_test_data['Volume'],
-    'Issue': parse_test_data['Issue'],
-    'Pagination': parse_test_data['Pagination'],
-    'Title': parse_test_data['Title'],
-    'PubYear': parse_test_data['PubYear']
-    }
+    component_names = ['Authors', 'Journal', 'Volume', 'Issue', 'Pagination', 'Title', 'PubYear']
+    actual_components_list = []
+    for _,reference_test_data in parse_test_data.iterrows():
+        a = {}
+        for component_name in component_names:
+            a.update({component_name : reference_test_data[component_name]})
+        actual_components_list.append(a)
 
-    test_info = parse_test_data
-    test_scores = evaluate_metric(merged_components, parse_test_data['Predicted merged components'])
+    test_scores = evaluate_metric(actual_components_list, parse_test_data['Predicted merged components'].tolist(), levenshtein_threshold)
 
     return test_scores
--- policytool/refparse/evaluate_algo.py
+++ policytool/refparse/evaluate_algo.py
@@ -123,7 +123,7 @@ if __name__ == '__main__':
         )
 
     logger.info('[+] Running test 4')
-    test4_scores = evaluate_parse(parse_test_data, model)
+    test4_scores = evaluate_parse(parse_test_data, model, settings.LEVENSHTEIN_DIST_THRESHOLD)
 
     logger.info('[+] Running test 5')
     test5_scores = evaluate_match_references(match_publications, test_publications, settings.FUZZYMATCH_THRESHOLD)
