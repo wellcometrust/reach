d90482510698f6c988316548d92faeec78166970
Elizabeth Gallagher <lizgzil@hotmail.com>
2019-05-17 17:36:16 +0100

    Removing the csv for the scraper tests and adding the folder names where the evaluation data is for the scraper evaluation

--- policytool/refparse/algo_evaluation/evaluate_settings.py
+++ policytool/refparse/algo_evaluation/evaluate_settings.py
@@ -9,7 +9,8 @@ class TestSettings(BaseSettings):
     FOLDER_PREFIX = "./algo_evaluation/data_evaluate"
     LOG_FILE_PREFIX = './algo_evaluation/results'
 
-    SCRAPE_DATA_FILE_NAME = "scrape_test_data.csv"
+    SCRAPE_DATA_PDF_FOLDER_NAME = "pdfs"
+    SCRAPE_DATA_REF_PDF_FOLDER_NAME = "pdf_sections"
 
     NUM_REFS_FILE_NAME = "split_section_test_data.csv"
     NUM_REFS_TEXT_FOLDER_NAME = "scraped_references_sections"
