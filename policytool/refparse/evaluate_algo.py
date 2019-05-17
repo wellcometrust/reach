92e9bcfd7370a77f705c1dde97efe5635412f5b6
Elizabeth Gallagher <lizgzil@hotmail.com>
2019-05-17 17:38:04 +0100

    Allowing get_references_sections to look for txt files that might not exist and not breaking, adding get_sections_text_dict - a function to go into all the text files of the scraper evaluation pdfs and put them into a nested dictionary, inputting new scraper evaluation data format

--- policytool/refparse/evaluate_algo.py
+++ policytool/refparse/evaluate_algo.py
@@ -4,6 +4,7 @@ e.g. python test_algo.py --verbose True
 
 from argparse import ArgumentParser
 import os
+from os import listdir 
 
 from utils import FileManager
 from datetime import datetime
@@ -20,10 +21,56 @@ def get_references_sections(filenames, foldername):
 
     references_sections = []
     for filename in filenames:
-        section = open("{}/{}.txt".format(foldername, filename)).read()
+        try:
+            section = open("{}/{}.txt".format(foldername, filename)).read()
+        except:
+            section = ""
         references_sections.append(section)
 
     return references_sections
+
+def get_sections_text_dict(pdf_names, sections_folder_names, sections_location):
+    """
+    For each pdf in the evaluation data, get all the text from each txt file in the location given
+    
+    Input:
+    pdf_names : A list of the pdf names in the evaluation data 
+                e.g [pdf_name1, pdf_name2, ...]
+    sections_folder_names : 
+                A list of the folder names for each section we have evaluation data for
+                e.g. ['reference', 'bibliograph']
+    sections_location : 
+                The file location where each of the section folders are kept
+                e.g './algo_evaluation/data_evaluate/pdf_sections'
+
+    Output:
+    sections_dict : 
+                A nested dictionary of all the sections text for each pdf in the evaluation data,
+                in form {pdf_name1 : {section1 : 'text', section2 : 'text', ... },
+                        pdf_name2 : {section1 : 'text', section2 : 'text', ... }, ...}
+    """
+
+    # Get the text for each section for each pdf
+    sections = [pdf_names]
+    for section_folder in sections_folder_names:
+        section_text = get_references_sections(
+            pdf_names,
+            '{}/{}'.format(
+                sections_location,
+                section_folder
+                )
+            )
+        sections.append(section_text)
+
+    sections_dict = {}
+    for p, *r in zip(*sections):
+        sections = {}
+        for i, section_folder in enumerate(sections_folder_names):
+            sections.update({section_folder : r[i]})
+        sections_dict.update({p: sections})
+
+    return sections_dict
+
 
 def create_argparser():
     parser = ArgumentParser()
@@ -58,12 +105,18 @@ if __name__ == '__main__':
     fm = FileManager()
 
     # Load data to test scraping for tests 1 and 2:
-    logger.info('[+] Reading {}'.format(settings.SCRAPE_DATA_FILE_NAME))
-    scrape_test_data = fm.get_file(
-        settings.SCRAPE_DATA_FILE_NAME,
-        settings.FOLDER_PREFIX,
-        'csv'
-    )
+    logger.info('[+] Reading {}'.format(settings.SCRAPE_DATA_PDF_FOLDER_NAME))
+
+    # Get the pdf names from the evaluate scraping folder:
+    scrape_pdf_location = '{}/{}'.format(settings.FOLDER_PREFIX,settings.SCRAPE_DATA_PDF_FOLDER_NAME)
+    pdf_names = listdir(scrape_pdf_location)
+    pdf_names = [os.path.splitext(pdf_name)[0] for pdf_name in pdf_names]
+    pdf_names.remove('.DS_Store')
+
+    sections_location = '{}/{}'.format(settings.FOLDER_PREFIX,settings.SCRAPE_DATA_REF_PDF_FOLDER_NAME)
+    sections_folder_names = [name for name in os.listdir(sections_location) if os.path.isdir('{}/{}'.format(sections_location, name))]
+
+    scrape_test_data = get_sections_text_dict(pdf_names, sections_folder_names, sections_location)
 
     # Load data to test split sections for test 3:
     logger.info('[+] Reading {}'.format(settings.NUM_REFS_FILE_NAME))
@@ -110,8 +163,9 @@ if __name__ == '__main__':
 
     logger.info('\nStarting the tests...\n')
 
-    logger.info('[+] Running tests 1 and 2')
-    test1_scores, test2_scores = evaluate_find_section(scrape_test_data)
+    logger.info('[+] Running tests 1 and 2')                     
+
+    test1_scores, test2_scores = evaluate_find_section(scrape_test_data, scrape_pdf_location)
 
     logger.info('[+] Running test 3')
     test3_scores = evaluate_split_section(
