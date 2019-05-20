92a9a2ccaddd9450fe22d13e6535cf1b60432761
Elizabeth Gallagher <lizgzil@hotmail.com>
2019-05-20 14:39:52 +0100

    Addres Nick's comments - Simplify and make clearer evaluate_parse and get_levenshtein_df, rename variables, use multiple lines

--- policytool/refparse/algo_evaluation/evaluate_parse.py
+++ policytool/refparse/algo_evaluation/evaluate_parse.py
@@ -1,10 +1,10 @@
 
-import pandas as pd
-import numpy as np
-import editdistance
-
-from utils.parse import structure_reference
 from sklearn.metrics.pairwise import cosine_similarity
+import editdistance
+import numpy as np
+import pandas as pd
+
+from policytool.refparse.utils.parse import structure_reference
 
 def get_levenshtein_df(df1, df2):
     """
@@ -13,51 +13,49 @@ def get_levenshtein_df(df1, df2):
     """
 
     calc_lev_dist_column = lambda s1, s2:\
-                            editdistance.eval(s1, s2)/max(len(s1), len(s2))\
-                            if (s1!='' or s2 !='')\
-                            else 0
+                        editdistance.eval(s1, s2)/max(len(s1), len(s2))\
+                        if (s1!='' or s2 !='')\
+                        else 0
+
+    lev_list = [
+        calc_lev_dist_column(a,p) for (a,p) in zip(df1.stack(), df2.stack())
+    ]
     lev_dist = pd.DataFrame(
-                [df1[column].combine(df2[column], calc_lev_dist_column) for column in df1.columns]
-                ).T
+        np.reshape(lev_list, df1.shape),
+        columns = df1.columns
+    )
 
     return lev_dist
 
-def evaluate_metric(actual_components_list, predicted_components_list, levenshtein_threshold):
+def evaluate_metric(
+        actual_categories, predicted_categories, levenshtein_threshold):
 
-    # For the computations below it's useful for any nan's to be blank strings
-    df1 = pd.DataFrame(actual_components_list).replace(np.nan,'').astype(str)
-    df2 = pd.DataFrame(predicted_components_list).replace(np.nan,'').astype(str)
+    equal = actual_categories == predicted_categories
 
-    df1['PubYear'] = [d[0:4] if d!='' else d for d in df1['PubYear']]
+    accuracy_cat = equal.mean()
+    accuracy = equal.values.mean()
 
-    equal = df1 == df2
-
-    proportion_equal_cat = equal.sum()/len(equal)
-    proportion_equal = (equal.sum().sum())/(equal.shape[0]*equal.shape[1])
-
-    lev_dist = get_levenshtein_df(df1, df2)
+    lev_dist = get_levenshtein_df(actual_categories, predicted_categories)
 
     quite_equal = lev_dist<levenshtein_threshold
-    proportion_quite_equal_cat = quite_equal.sum()/len(quite_equal)
-    proportion_quite_equal = (quite_equal.sum().sum())/(quite_equal.shape[0]*quite_equal.shape[1])
+    accuracy_quite_equal_cat = quite_equal.mean()
+    accuracy_quite_equal = quite_equal.values.mean()
 
     test_scores = {
-            'Score' : proportion_equal,
-            'Proportion of components predicted correctly (macro)'\
-                                                    : proportion_equal,
-            'Proportion of components predicted correctly (micro)'\
-                                                    : proportion_equal_cat,
-            'Mean normalised Levenshtein distance (macro)'\
-                                                    : lev_dist.mean().mean(),
-            'Mean normalised Levenshtein distance (micro)'\
-                                                    : lev_dist.mean(),
-            'Proportion of components predicted almost correctly\
-            (normalised Levenshtein < {}) (macro)'.format(levenshtein_threshold)\
-                                                    : proportion_quite_equal,
-            'Proportion of components predicted almost correctly\
-            (normalised Levenshtein < {}) (micro)'.format(levenshtein_threshold)\
-                                                    : proportion_quite_equal_cat
-            }
+        'Score' : accuracy,
+        'Strict accuracy (micro)': accuracy,
+        'Strict accuracy (per category)': accuracy_cat,
+        'Lenient accuracy (micro)\
+        (normalised Levenshtein < {})'.format(levenshtein_threshold) :\
+            accuracy_quite_equal,
+        'Lenient accuracy (per category)\
+        (normalised Levenshtein < {}) ()'.format(levenshtein_threshold) :\
+            accuracy_quite_equal_cat,
+        'Mean normalised Levenshtein distance (all categories)' :\
+            lev_dist.mean().mean(),
+        'Mean normalised Levenshtein distance (per category)' :\
+            lev_dist.mean()
+    }
 
     return test_scores
 
@@ -68,19 +66,18 @@ def evaluate_parse(parse_test_data, model, levenshtein_threshold):
         structured_reference = structure_reference(model, reference)
         predicted_structure.append(structured_reference)
 
-    parse_test_data['Predicted merged components'] = predicted_structure
+    # For the evaluation calculations it's useful for any nan's to be blank strings
+    predicted_categories = pd.DataFrame(predicted_structure).replace(np.nan,'').astype(str)
+    # Get category names from the predicted categories
+    # Important so the actual and predicted are in the same order
+    category_names = predicted_categories.columns
+    actual_categories = parse_test_data[category_names].replace(np.nan,'').astype(str)
 
-    component_names = ['Authors', 'Journal', 'Volume', 'Issue', 'Pagination', 'Title', 'PubYear']
-    actual_components_list = []
-    for _,reference_test_data in parse_test_data.iterrows():
-        a = {}
-        for component_name in component_names:
-            a.update({component_name : reference_test_data[component_name]})
-        actual_components_list.append(a)
+    actual_categories['PubYear'] = [d[0:4] if d!='' else d for d in actual_categories['PubYear']]
 
     test_scores = evaluate_metric(
-                    actual_components_list,
-                    parse_test_data['Predicted merged components'].tolist(),
+                    actual_categories,
+                    predicted_categories,
                     levenshtein_threshold
                     )
 
