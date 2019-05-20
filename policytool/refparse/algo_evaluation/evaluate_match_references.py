e690a643744ac496cf072e8b2a15ff967c1b9a13
Elizabeth Gallagher <lizgzil@hotmail.com>
2019-05-20 12:13:16 +0100

    Address Nick's comments - rename variables, return frequency matrix of match types

--- policytool/refparse/algo_evaluation/evaluate_match_references.py
+++ policytool/refparse/algo_evaluation/evaluate_match_references.py
@@ -6,43 +6,48 @@ from utils import FuzzyMatcher
 
 def evaluate_metric(actual, predicted):
 
-    similarity = round(f1_score(actual, predicted, average='micro'), 3)
+    actual_binary = actual.astype(int)
+    # predicted is True, False and "No match found",
+    # convert to 1 (True) or 0 (False or "No match found")
+    predicted_binary = [1 if pred_type==True else 0 for pred_type in predicted]
+
+    similarity = round(f1_score(actual_binary, predicted_binary, average='micro'), 3)
 
     test_scores = {
         'Score' : similarity,
         'Micro average F1-score' : similarity,
-        'Classification report' : classification_report(actual, predicted) 
+        'Classification report' : classification_report(actual_binary, predicted_binary),
+        'Frequency table of match types' : pd.crosstab(index=actual, columns=predicted) 
         }
 
     return test_scores
 
-def evaluate_match_references(match_publications, test_publications, match_threshold):
+def evaluate_match_references(publications, evaluation_references, match_threshold):
+    """
+    Try to match the evaluation_references with references in 'publications'
+    """
 
     # Get the data into the same format as would be
-    test_publications = test_publications.rename(columns={'Match title': 'Title', 'Match pub id': 'Reference id'})
-    test_publications['Journal'] = None
-    test_publications['PubYear'] = None
-    test_publications['Authors'] = None
-    test_publications['Issue'] = None
-    test_publications['Volume'] = None
-    test_publications['Document id'] = range(0, len(test_publications))
-    test_publications['Document uri'] = None
+    evaluation_references = evaluation_references.rename(columns={'Match title': 'Title', 'Match pub id': 'Reference id'})
+    evaluation_references['Document id'] = range(0, len(evaluation_references))
 
     # Take out the negative reference id's (so you shouldn't find them)
-    neg_test_pub_ids = test_publications.loc[test_publications['Type']==0]['Reference id']
-    match_publications_no_negs = match_publications.loc[~match_publications['uber_id'].isin(neg_test_pub_ids)]
+    neg_test_pub_ids = evaluation_references.loc[evaluation_references['Do we expect a match?']==0]['Reference id']
+    publications_no_negs = publications.loc[~publications['uber_id'].isin(neg_test_pub_ids)]
 
-    fuzzy_matcher = FuzzyMatcher(match_publications_no_negs, match_threshold)
+    fuzzy_matcher = FuzzyMatcher(publications_no_negs, match_threshold)
 
-    match_data = fuzzy_matcher.match_vectorised(test_publications)
+    matched_publications = fuzzy_matcher.match_vectorised(evaluation_references)
 
-    # Does each publication have the correct match (True) or not (False)
-    # (There might not be matches found for all the test_publications)
-    merged_test_pred = pd.merge(test_publications, match_data, how="left", left_on='Reference id', right_on='Reference id')
-    match_correct = merged_test_pred['Reference id']==merged_test_pred['uber_id']
+    evaluation_matches = pd.merge(evaluation_references, matched_publications, how="left", left_on='Reference id', right_on='Reference id')
 
-    merged_test_pred['Matched correctly?'] = match_correct
+    # Does each publication have the correct match (true), an incorrect match (false) or no match
+    evaluation_matches['Matched correctly?'] = [
+        matches['Reference id']==matches['uber_id'] if not pd.isnull(matches['uber_id'])\
+        else "No match found"\
+        for _, matches in evaluation_matches.iterrows()
+        ]
 
-    test_scores = evaluate_metric(merged_test_pred['Type'].astype(int), merged_test_pred['Matched correctly?'].astype(int))
+    test_scores = evaluate_metric(evaluation_matches['Do we expect a match?'], evaluation_matches['Matched correctly?'])
 
     return test_scores
