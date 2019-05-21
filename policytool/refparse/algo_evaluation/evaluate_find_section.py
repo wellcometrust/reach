114d71b1a6a4eb495b24a8e4945454f384e30030
Elizabeth Gallagher <lizgzil@hotmail.com>
2019-05-21 16:32:26 +0100

    Add confusion matrix output to evaluate_find_section, neaten up some of the functions and rename variables

--- policytool/refparse/algo_evaluation/evaluate_find_section.py
+++ policytool/refparse/algo_evaluation/evaluate_find_section.py
@@ -1,20 +1,33 @@
-import sys
-sys.path.append("..")
-from pdf_parser.pdf_parse import parse_pdf_document, grab_section
-
-import os.path
+from sklearn.metrics import classification_report, f1_score, confusion_matrix
+import editdistance
 import pandas as pd
 import numpy as np
 
-from sklearn.metrics import classification_report, f1_score
-import editdistance
+import os.path
+import sys
 
-def evaluate_metric_scraped(actual, predicted, section_names):
+sys.path.append("..")
+from pdf_parser.pdf_parse import parse_pdf_document, grab_section
+
+
+def pretty_confusion_matrix(actual_data, predict_data, labels):
+    cm  = confusion_matrix(actual_data, predict_data, labels = labels)
+    pretty_conf = pd.DataFrame(
+        cm,
+        columns=["Predicted {}".format(label) for label in labels],
+        index=["Actually {}".format(label) for label in labels]
+        )
+    return pretty_conf
+
+def evaluate_metric_scraped(actual, predicted, sections):
     """
     Input:
         actual : a boolean list of whether section text was in the pdf
         predicted : a boolean list of whether section text was scraped
-        section_names : a list of the section names
+        sections : a list of the section names for each actual/predicted pair
+    Output:
+        Various metrics for how accurately the scraper scraped a
+        section or not, no comment on how good the scrape was though
     """
 
     similarity = round(f1_score(actual, predicted, average='micro'), 3)
@@ -22,117 +35,142 @@ def evaluate_metric_scraped(actual, predicted, section_names):
     test_scores = {
         'Score' : similarity,
         'Micro average F1-score' : similarity,
-        'Classification report' : classification_report(actual, predicted) 
+        'Classification report' : classification_report(actual, predicted),
+        'Confusion matrix' : pretty_confusion_matrix(
+                actual, predicted, [True, False]
+            )
         }
 
-    for section_name in set(section_names):
-        has_section = [e for i,e in enumerate(actual) if section_names[i] == section_name]
-        has_scraped_section = [e for i,e in enumerate(predicted) if section_names[i] == section_name]
-        test_scores["Classification report for the {} section".format(section_name)] = classification_report(has_section, has_scraped_section)
+    for section_name in set(sections):
+        actual_section = [
+            a for (s,a) in zip(sections, actual) if s == section_name
+        ]
+        predicted_section = [
+            p for (s,p) in zip(sections, predicted) if s == section_name
+        ]
+        test_scores["Classification report for the {} section".format(
+            section_name
+            )] = classification_report(actual_section, predicted_section)
+        test_scores["Confusion matrix for the {} section".format(
+            section_name
+            )] = pretty_confusion_matrix(
+                    actual_section, predicted_section, [True, False]
+                )
 
     return test_scores
 
 
-def evaluate_metric_quality(actual, predicted, sections, levenshtein_threshold):
+def evaluate_metric_quality(scrape_data, levenshtein_threshold):
     """
-    Text similarity for when there is a section (actual!='')
+    Normalised Levenshtein distances between actual and predicted section text
+    for pdfs where there is a section (actual!='')
     """
 
-    section_texts_all = [{'Actual section text' : a, 'Predicted section text' : p, 'Section' : s} for a,p,s in zip(actual, predicted, sections)]
+    # Get rid of times when there is no section
+    scrape_data = list(filter(lambda x: x['Actual text'] != '', scrape_data))
 
-    section_texts_all = list(filter(lambda x: x['Actual section text'] != '', section_texts_all))
+    actual_texts = [s['Actual text'] for s in scrape_data]
+    predicted_texts = [s['Predicted text'] for s in scrape_data]
+    sections = [s['Section'] for s in scrape_data]
 
-    lev_distance = []
-    for section_texts in section_texts_all:
-        actual_text = section_texts['Actual section text']
-        predicted_text = section_texts['Predicted section text']
-        lev_distance.append(editdistance.eval(actual_text, predicted_text) / max(len(actual_text), len(predicted_text)))
+    # Get all the normalised Lev distances
+    lev_distances = [
+        editdistance.eval(actual_text, predicted_text) / 
+        max(len(actual_text), len(predicted_text)) \
+        for (actual_text, predicted_text) in 
+            zip(actual_texts, predicted_texts)
+    ]   
 
-    # Sections for those that actually exist
-    sections_exist = [s['Section'] for s in section_texts_all]
-
-    # Which sections (of the texts that actual exist) were found exactly?
-    equal = [l==0 for l in lev_distance]
+    # Which sections were found exactly?
+    equal = [lev_distance == 0 for lev_distance in lev_distances]
     
-    # Which sections (of the texts that actual exist) were found roughly the same?
-    quite_equal = [l<levenshtein_threshold  for l in lev_distance]
+    # Which sections were found roughly the same?
+    quite_equal = [
+        lev_distance<levenshtein_threshold  for lev_distance in lev_distances
+    ]
 
     test_scores = {
-        'Score' : sum(equal)/len(equal),
-        'Mean normalised Levenshtein distance' : np.mean(lev_distance),
-        'Strict accuracy (micro)' : sum(equal)/len(equal),
-        'Lenient accuracy (micro)' : sum(quite_equal)/len(quite_equal)}
+        'Score' : np.mean(equal),
+        'Mean normalised Levenshtein distance' : np.mean(lev_distances),
+        'Strict accuracy (micro)' : np.mean(equal),
+        'Lenient accuracy (micro)' : np.mean(quite_equal)}
 
-    strict_accuracies = []
-    lenient_accuracies = []
-    for section_name in set(sections_exist):
-        lev_distance_section = [e for i,e in enumerate(lev_distance) if sections_exist[i] == section_name]
-        equal_section = [l==0 for l in lev_distance_section]
-        quite_equal_section = [l<levenshtein_threshold  for l in lev_distance_section]
-        strict_acc_section = sum(equal_section)/len(equal_section)
-        lenient_acc_section = sum(quite_equal_section)/len(quite_equal_section)
+    for section_name in set(sections):
+        # Get the Levenshtein distances for this sections actual-predicted pairs
+        lev_distances_section = [
+                lev_distance for (section,lev_distance) \
+                in zip(sections, lev_distances) \
+                if section == section_name
+            ]
 
-        test_scores['Mean normalised Levenshtein distance for the {} section'.format(section_name)] = np.mean(lev_distance_section)
-        test_scores['Strict accuracy for the {} section'.format(section_name)] = strict_acc_section
-        test_scores['Lenient accuracy for the {} section'.format(section_name)] = lenient_acc_section
+        equal_section = [l==0 for l in lev_distances_section]
+        quite_equal_section = [
+            l<levenshtein_threshold  for l in lev_distances_section
+        ]
+        strict_acc_section = np.mean(equal_section)
+        lenient_acc_section = np.mean(quite_equal_section)
 
-        strict_accuracies.append(strict_acc_section)
-        lenient_accuracies.append(lenient_acc_section)
-
-    test_scores['Strict accuracy (macro)'] = np.mean(strict_accuracies)
-    test_scores['Lenient accuracy (macro)'] = np.mean(lenient_accuracies)
+        test_scores[
+            'Mean normalised Levenshtein distance for the {} section'.format(
+                section_name
+                )
+            ] = np.mean(lev_distances_section)
+        test_scores[
+            'Strict accuracy for the {} section'.format(section_name)
+            ] = strict_acc_section
+        test_scores[
+            'Lenient accuracy for the {} section'.format(section_name)
+            ] = lenient_acc_section
     
     return test_scores
 
-def scrape_pdfs(scrape_test_data, scrape_pdf_location):
+def scrape_process_pdf(
+        section_names, pdf_name, scrape_pdf_location, actual_texts
+        ):
     """
-    Input
-    scrape_test_data: A nested dictionary of all the sections text for each pdf in the evaluation data,
-                in form {pdf_name1 : {section1 : 'text', section2 : 'text', ... },
-                        pdf_name2 : {section1 : 'text', section2 : 'text', ... }, ...}
-    Output
-    scrape_test_data: the evaluation dataframe now with whether a references section was
-        scraped or not, the text scraped for each of the 'sections' sections, and a re-formatted
-        actual references section text (in dictionary format)
+    Input:
+        section_names :  the list of sections we are looking for in the pdf
+        pdf_name : the name of the pdf
+        scrape_pdf_location : the file location of the pdf
+    Output:
+        scrape_data : a list of dicts with the predicted and actual texts for
+            each of the sections we looked for in the pdf
     """
+    with open('{}/{}.pdf'.format(scrape_pdf_location, pdf_name), 'r') as f:
+        pdf_file, full_text = parse_pdf_document(f)
+        scrape_data = []
+        for section_name in section_names:
+            scrape_data.append({
+                'File' : pdf_name,
+                'Section' : section_name,
+                'Predicted text' : grab_section(pdf_file, section_name),
+                'Actual text' : actual_texts[section_name]})
+    return scrape_data
 
-    # Predict the references text for these pdfs
 
-    sections = scrape_test_data[next(iter(scrape_test_data))].keys()
-    text_results = {}
-    for pdf_name in scrape_test_data.keys():
-        with open('{}/{}.pdf'.format(scrape_pdf_location, pdf_name), 'r') as f:
-            pdf_file, full_text = parse_pdf_document(f)
-            section_dict = {}
-            for section in sections:
-                section_text = grab_section(pdf_file, section)
-                section_dict.update({section : section_text})
-            text_results.update({pdf_name : section_dict})
+def evaluate_find_section(
+        evaluate_find_section_data, scrape_pdf_location, levenshtein_threshold
+        ):
 
-    return text_results
-
-def evaluate_find_section(scrape_test_data, scrape_pdf_location):
-    levenshtein_threshold = 0.3
-
-    text_results = scrape_pdfs(scrape_test_data, scrape_pdf_location)
-
-    section_names = []
-    predicted_section = []
-    actual_section = []
-    for pdf_names in scrape_test_data.keys():
-        predicted_sections = text_results[pdf_names]
-        actual_sections = scrape_test_data[pdf_names]
-        for sections in actual_sections.keys():
-            predicted_section.append(predicted_sections[sections])
-            actual_section.append(actual_sections[sections])
-            section_names.append(sections)
+    # Get the predicted text for each of the pdf sections for each pdf
+    section_names = evaluate_find_section_data[
+        next(iter(evaluate_find_section_data))
+        ].keys()
+    scrape_data = []
+    for pdf_name, actual_texts in evaluate_find_section_data.items():
+        scrape_data.extend(
+            scrape_process_pdf(section_names, pdf_name, scrape_pdf_location, actual_texts)
+            ) 
 
     test1_scores = evaluate_metric_scraped(
-        [a!='' for a in actual_section], [p!='' for p in predicted_section],
-        section_names
+        [pred_section['Actual text']!='' for pred_section in scrape_data],
+        [pred_section['Predicted text']!='' for pred_section in scrape_data],
+        [pred_section['Section'] for pred_section in scrape_data]
         )
 
-    test2_scores = evaluate_metric_quality(actual_section, predicted_section, section_names, levenshtein_threshold)
+    test2_scores = evaluate_metric_quality(
+        scrape_data,
+        levenshtein_threshold)
 
     return test1_scores, test2_scores
 
