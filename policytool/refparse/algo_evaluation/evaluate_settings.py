29682ecbfe62d110ac69e46b1b7515a9637a22be
Elizabeth Gallagher <lizgzil@hotmail.com>
2019-05-23 17:25:45 +0100

    Some fixes and neatening to the evaluation work

--- policytool/refparse/algo_evaluation/evaluate_settings.py
+++ policytool/refparse/algo_evaluation/evaluate_settings.py
@@ -3,27 +3,28 @@ from settings import BaseSettings
 
 class TestSettings(BaseSettings):
 
-    SPLIT_SECTION_SIMILARITY_THRESHOLD = 40
-
-    LEVENSHTEIN_DIST_THRESHOLD = 0.3
-    LEVENSHTEIN_DIST_SCRAPER_THRESHOLD = 0.3
-
     FOLDER_PREFIX = "./algo_evaluation/data_evaluate"
     LOG_FILE_PREFIX = './algo_evaluation/results'
 
+    # Variables for find section evaluation data
+    LEVENSHTEIN_DIST_SCRAPER_THRESHOLD = 0.3
     SCRAPE_DATA_PDF_FOLDER_NAME = "pdfs"
     SCRAPE_DATA_REF_PDF_FOLDER_NAME = "pdf_sections"
 
+    # Variables for split section evaluation data
+    SPLIT_SECTION_SIMILARITY_THRESHOLD = 40
     NUM_REFS_FILE_NAME = "split_section_test_data.csv"
     NUM_REFS_TEXT_FOLDER_NAME = "scraped_references_sections"
 
+    # Variables for parse evaluation data
+    LEVENSHTEIN_DIST_PARSE_THRESHOLD = 0.3
     MODEL_FILE_TYPE = 'pickle'
     MODEL_FILE_PREFIX = './reference_parser_models/'
     MODEL_FILE_NAME = 'reference_parser_pipeline.pkl'
     PARSE_REFERENCE_FILE_NAME = "actual_reference_structures_sample.csv"
 
+    # Variables for match evaluation data
     TEST_PUB_DATA_FILE_NAME = "positive_and_negative_match_test_data.csv"
-
     MATCH_PUB_DATA_FILE_NAME = "uber_api_publications.csv"
 
 settings = TestSettings()
