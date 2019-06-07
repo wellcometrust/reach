59b14ef880b54a1e6aa8febc59f5a0390e5bb58b
Elizabeth Gallagher <lizgzil@hotmail.com>
2019-06-07 16:20:37 +0100

    Change split section to use the IOBE line model not the regex

--- policytool/refparse/utils/split.py
+++ policytool/refparse/utils/split.py
@@ -1,35 +1,25 @@
 # -*- coding: utf-8 -*-
 
+import dill
 import re
 
+from sklearn.pipeline import Pipeline
 
-def split_section(references_section, regex="\n"):
-    """Converts the unstructured text into reference components
-    Input:
-    - a References section string
-    Output:
-    - A list of references data which come into a dict with keys
-        about the reference string, reference id, document uri and
-        document id
-    Nomenclature:
-    Document > References
-    """
-    references = re.split(regex, references_section)
-    # Remove any row with less than 10 characters in
-    # (these can't be references)
-    references = [r for r in references if len(r) > 10]
-    # delete '-\n', convert \n to spaces, convert â€™ to ', convert â€“ to -
-    references = [
-        reference.replace(
-            "-\n", ""
-        ).replace(
-            "\n", " "
-        ).replace(
-            "â€™", "'"
-        ).replace(
-            "â€“", "-"
-        )
-        for reference in references
-    ]
+def split_section(references_section, model):
+
+    lines = references_section.split('\n')
+
+    line_beginnings = [m.start() for m in re.finditer(r"\n", references_section)]
+    if references_section[0] != "\n":
+        line_beginnings = [0] + line_beginnings
+
+    predict_tags = model.predict(lines)
+    prob_tags = [p.max() for p in model.predict_proba(lines)]
+
+    # Cut up references_section at the line beginning of a line predicted to be a B
+    cut_here = [wb for (wb, t, p) in zip(line_beginnings, predict_tags, prob_tags) if t == "B"]
+
+    # Cut references_section at each index in cut_here:
+    references = [references_section[i:j] for i,j in zip(cut_here, cut_here[1:] + [None])]
 
     return references
