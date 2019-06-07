c32b1028092891cc48a81be58e2ca9b06e14d652
Elizabeth Gallagher <lizgzil@hotmail.com>
2019-06-07 16:18:33 +0100

    Add model to split section test

--- policytool/refparse/tests/test_split.py
+++ policytool/refparse/tests/test_split.py
@@ -1,17 +1,24 @@
 import unittest
 
+import dill
+import os
+
 from policytool.refparse.utils import split_section
 from policytool.refparse.refparse import SectionedDocument
 
+MODELS_DIR = os.path.join(os.path.dirname(__file__), '..', 'reference_splitter_models')
+
+with open(os.path.join(MODELS_DIR, "line_iobe_pipeline_20190502.dll"), "rb") as f:
+    model = dill.load(f)
 
 class TestSplit(unittest.TestCase):
 
     def test_empty_sections(self):
-        references = split_section("")
+        references = split_section("", model)
         self.assertEqual(references, [], "Should be []")
 
     def test_oneline_section(self):
-        references = split_section("This is one line")
+        references = split_section("This is one line", model)
         self.assertEqual(
             references,
             ["This is one line"],
@@ -19,12 +26,13 @@ class TestSplit(unittest.TestCase):
         )
 
     def test_empty_lines_section(self):
-        references = split_section("\n\n\n")
+        references = split_section("\n\n\n", model)
         self.assertEqual(references, [], "Should be []")
 
     def test_normal_section(self):
         references = split_section(
-            "One reference\nTwo references\nThree references\n"
+            "One reference\nTwo references\nThree references\n",
+            model
         )
         self.assertEqual(
             references,
