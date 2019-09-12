b4fd81be1a042b7fb9ca892910c9e58d86cfa8fa
SamDepardieu <sam.depardieu@gmail.com>
2019-09-12 16:50:50 +0100

    Add an optionnal prefix to the elasticsearch index
    The index used in Elasticsearch to store epmc metadata and fulltexts is
    the same for both test and main dag at the moment. This commit adds a
    "test_" prefix before the test dag indexes.

--- policytool/airflow/tasks/es_index_fulltext_docs.py
+++ policytool/airflow/tasks/es_index_fulltext_docs.py
@@ -7,8 +7,9 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 from policytool.airflow.hook.wellcome_s3_hook import WellcomeS3Hook
-from policytool.elastic import fulltext_docs 
+from policytool.elastic import fulltext_docs
 import policytool.elastic.common
+
 
 class ESIndexFulltextDocs(BaseOperator):
     """ Download the latest scraped publications texts stored in S3 and index
@@ -22,7 +23,7 @@ class ESIndexFulltextDocs(BaseOperator):
     @apply_defaults
     def __init__(self, src_s3_key, es_host, organisation, es_port=9200,
                  max_items=None, aws_conn_id='aws_default',
-                 *args, **kwargs):
+                 es_index_prefix=None, *args, **kwargs):
         """
         Args:
             src_s3_key: S3 URL for the json.gz output file.
@@ -43,6 +44,7 @@ class ESIndexFulltextDocs(BaseOperator):
         self.organisation = organisation
         self.aws_conn_id = aws_conn_id
         self.max_items = max_items
+        self.es_index_prefix = es_index_prefix
 
     def execute(self, context):
         es = policytool.elastic.common.connect(
@@ -73,6 +75,7 @@ class ESIndexFulltextDocs(BaseOperator):
                 tf,
                 es,
                 self.organisation,
-                max_items=self.max_items
+                max_items=self.max_items,
+                es_index_prefix=self.es_index_prefix,
             )
         self.log.info('import complete count=%d', count)
--- policytool/elastic/epmc_metadata.py
+++ policytool/elastic/epmc_metadata.py
@@ -3,31 +3,34 @@ Inserts EPMC metadata into Elasticsearch.
 
 Sample URL for testing:
 
-    s3://datalabs-staging/airflow/output/open-research/epmc-metadata/epmc-metadata.json.gz 
+    s3://datalabs-staging/airflow/output/open-research/epmc-metadata/epmc-metadata.json.gz
 """
 
 import json
 import logging
+import functools
 
 from . import common
 
 ES_INDEX = 'epmc-metadata'
 CHUNK_SIZE = 1000  # tuned for small(ish) size of pub metadata
 
-def to_es_action(line):
+
+def to_es_action(es_index, line):
     d = json.loads(line)
     return {
-        "_index": ES_INDEX,
+        "_index": es_index,
         "doc": d,
     }
 
 
-def clean_es(es):
+def clean_es(es, es_index_prefix):
     """ Ensure an empty index exists. """
-    common.clean_es(es, ES_INDEX)
+    es_index = es_index_prefix + ES_INDEX
+    common.clean_es(es, es_index)
 
 
-def insert_file(f, es, max_items=None):
+def insert_file(f, es, es_index_prefix, max_items=None):
     """
     Inserts EPMC metadata from a json.gz file into Elasticsearch.
 
@@ -36,13 +39,18 @@ def insert_file(f, es, max_items=None):
         es: a living connection to elacticsearch
         max_items: maximum number of records to insert, or None
     """
+    if es_index_prefix:
+        es_index = es_index_prefix + ES_INDEX
+    else:
+        es_index = ES_INDEX
 
     logging.info(
         'epmc_metadata.insert_file: f=%s es=%s max_items=%s',
         f, es, max_items)
+    to_es_func = functools.partial(to_es_action, es_index)
     return common.insert_actions(
         es,
-        common.yield_actions(f, to_es_action, max_items),
+        common.yield_actions(f, to_es_func, max_items),
         CHUNK_SIZE,
         )
 
