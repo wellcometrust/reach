76e2108d01131f82f8b31ae4b17bf94fdf76f63c
SamDepardieu <sam.depardieu@gmail.com>
2019-09-13 14:55:10 +0100

    Add the ES prefix to the es_clean method as well

--- policytool/airflow/tasks/es_index_fulltext_docs.py
+++ policytool/airflow/tasks/es_index_fulltext_docs.py
@@ -52,7 +52,7 @@ class ESIndexFulltextDocs(BaseOperator):
         s3 = WellcomeS3Hook()
 
         # TODO: implement skipping mechanism
-        fulltext_docs.clean_es(es)
+        fulltext_docs.clean_es(es, self.es_index_prefix)
 
         if self.max_items:
             self.log.info(
