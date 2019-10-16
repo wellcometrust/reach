67de682dc19c01c2bee933658ce4ba15a1e8d31d
Matthew Upson <matthew.a.upson@gmail.com>
2019-10-16 19:24:14 -0300

    new: Add a newline at the end of each page (Fix #258)
    Prevents the issue of page numbers becoming concatenated with the first line of the following page.

--- policytool/scraper/tests/common.py
+++ policytool/scraper/tests/common.py
@@ -9,4 +9,5 @@ def get_path(p):
 
 TEST_PDF = get_path('pdfs/test_pdf.pdf')
 TEST_PDF_MULTIPAGE = get_path('pdfs/test_pdf_multipage.pdf')
+TEST_PDF_PAGE_NUMBER = get_path('pdfs/test_pdf_page_number.pdf')
 TEST_XML = get_path('xml/test_xml.xml')
