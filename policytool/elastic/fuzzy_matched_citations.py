8ffb018347a18188d1ba8c0c1394134dd2bdc7f3
SamDepardieu <sam.depardieu@gmail.com>
2019-10-25 10:58:26 +0100

    Apply the clean_es fix for fuzzy matched citation

--- policytool/elastic/fuzzy_matched_citations.py
+++ policytool/elastic/fuzzy_matched_citations.py
@@ -32,9 +32,21 @@ def to_es_action(org, es_index, line):
     }
 
 
-def clean_es(es, es_index):
-    """ Ensure an empty index exists. """
-    common.clean_es(es, es_index)
+def clean_es(es, es_index, org):
+    """ Ensure an empty index exists and delete documents from the
+    organisation to ensure no duplicates are inserted.
+    """
+    es_body = {
+        'query': {
+            'match': {
+                'organisation': org,
+            }
+        }
+    }
+    es.delete_by_query(
+        index=es_index,
+        body=es_body,
+    )
 
 
 def insert_file(f, es, org, es_index, max_items=None):
