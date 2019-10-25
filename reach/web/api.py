2f71933364a76a8b7c7ae4f051008906f43ed1bc
SamDepardieu <sam.depardieu@gmail.com>
2019-10-25 10:45:53 +0100

    Add new routes for a templates search results page

--- reach/web/api.py
+++ reach/web/api.py
@@ -11,12 +11,13 @@ import os.path
 from urllib.parse import urlparse
 
 import falcon
-import jinja2
 from elasticsearch import Elasticsearch
 
+from .templates import TemplateResource
 from . import search
 
 TEMPLATE_ROOT = os.path.join(os.path.dirname(__file__), 'templates')
+
 
 def configure_logger(logger):
     """ Configures our logger w/same settings & handler as our webserver
@@ -32,81 +33,12 @@ logger = logging.getLogger()
 configure_logger(logger)
 
 
-def to_template_names(path):
-    """
-    Maps HTTP request paths to Jinja template paths.
-
-    Args:
-        path: path portion of HTTP GET request
-
-    Returns:
-        Tuple of file paths that Jinja should search for.
-    """
-
-    if not path.startswith('/'):
-        raise ValueError
-    path = path[1:]  # remove leading /, jinja won't want it
-
-    if os.path.basename(path).startswith('_'):
-        # Macros are kept in templates starting with _; don't allow
-        # access to them.
-        return tuple()
-
-    if path == '':
-        return ('index.html',)
-
-    if path.endswith('/'):
-        return (
-            path[:-1] + '.html',
-            os.path.join(path, 'index.html'),
-        )
-
-    if path.endswith('.html'):
-        return (
-            path,
-            os.path.join(path[:-5], 'index.html'),
-        )
-
-    return (
-        path + '.html',
-        os.path.join(path, 'index.html'),
-    )
-
-
 def get_context(os_environ):
     return {
         'REACH_VERSION': os_environ.get(
             'REACH_VERSION', 'development'
         )
     }
-
-
-class TemplateResource:
-    """
-    Serves HTML templates. Note that templates are read from the FS for
-    every request.
-    """
-
-    def __init__(self, template_dir, context=None):
-        logger.info('TemplateResource: template_dir=%s', template_dir)
-        self.env = jinja2.Environment(
-            loader=jinja2.FileSystemLoader(template_dir),
-            autoescape=jinja2.select_autoescape(['html']),
-        )
-        if context is not None:
-            self.context = context
-        else:
-            self.context = {}
-
-    def on_get(self, req, resp):
-        tnames = to_template_names(req.path)
-        try:
-            template = self.env.select_template(tnames)
-            resp.body = template.render(**self.context)
-            resp.content_type = 'text/html'
-        except jinja2.TemplateNotFound:
-            resp.status = falcon.HTTP_404
-            return
 
 
 def create_api(conf):
@@ -135,12 +67,21 @@ def create_api(conf):
         TemplateResource(TEMPLATE_ROOT, get_context(os.environ))
     )
     api.add_route(
-        '/search',
+        '/search/citations',
         TemplateResource(TEMPLATE_ROOT, get_context(os.environ))
     )
     api.add_route(
+        '/search/content',
+        TemplateResource(TEMPLATE_ROOT, get_context(os.environ))
+    )
+    api.add_route(
+        '/results/content',
+        search.FulltextPage(TEMPLATE_ROOT, es, conf.es_explain,
+                            get_context(os.environ))
+    )
+    api.add_route(
         '/api/search',
-        search.Fulltext(es, conf.es_explain)
+        search.FulltextApi(es, conf.es_explain)
     )
     api.add_static_route('/static', conf.static_root)
     return api
