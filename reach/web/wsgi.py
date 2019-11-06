7563e7e676925f273315d411f4c20150c5f10622
SamDepardieu <sam.depardieu@gmail.com>
2019-11-06 11:18:39 +0000

    Add es indexes to the configuration

--- reach/web/wsgi.py
+++ reach/web/wsgi.py
@@ -16,11 +16,19 @@ class Configuration:
         if not self.es_host:
             raise Exception('No elasticsearch host')
         self.es_explain = os.environ.get('ELASTICSEARCH_EXPLAIN', False)
+        self.es_policy_docs_index = os.environ.get(
+            'ELASTICSEARCH_POLICYDOCS_INDEX',
+            'policy-docs'
+        )
+        self.es_citations_index = os.environ.get(
+            'ELASTICSEARCH_CITATIONS_INDEX',
+            'policy-citation'
+        )
 
         self.static_root = os.environ.get('STATIC_ROOT')
         if not self.static_root or not os.path.isdir(self.static_root):
             raise Exception(
-                "No static directory found. STATIC_ROOT=%r" % 
+                "No static directory found. STATIC_ROOT=%r" %
                 self.static_root
             )
 
