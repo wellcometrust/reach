72839b4488f8d785f6364222070077af56d9f66e
SamDepardieu <sam.depardieu@gmail.com>
2019-12-09 14:10:50 +0000

    Add a table querying js module

--- reach/web/src/js/app.js
+++ reach/web/src/js/app.js
@@ -1,7 +1,7 @@
 import clearSearch from './clearSearch.js';
-import sortTables from './sortTables.js';
+import sortTables from './elasticCommon.js';
 
 document.addEventListener('DOMContentLoaded', function(event) {
   clearSearch();
-  sortTables();
+  elasticCommon();
 });
