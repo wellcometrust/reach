cfd82b525395396b2f55d28aa4df10e5984d519d
Matthew Upson <matthew.a.upson@gmail.com>
2019-12-10 21:34:52 -0300

    chg: Update and refactor tests

--- reach/refparse/tests/test_fuzzy_match.py
+++ reach/refparse/tests/test_fuzzy_match.py
@@ -1,119 +1,98 @@
-import unittest
-
 import pandas as pd
+import pytest
 from pandas.util.testing import assert_frame_equal
-
-from reach.refparse.settings import settings
 from reach.refparse.utils import FuzzyMatcher
 
 
-class TestFuzzyMatcherInit(unittest.TestCase):
-    def test_empty_title(self):
-        real_publications = pd.DataFrame({
-            'title': []
-        })
-        threshold = 0.75
-        with self.assertRaises(ValueError):
-            FuzzyMatcher(real_publications, threshold)
+@pytest.fixture
+def fuzzy_matcher():
+    real_publications = [
+        {'title': 'Malaria', 'pmcid': 0},
+        {'title': 'Zika', 'pmcid': 1},
+    ]
+    fuzzy_matcher = FuzzyMatcher(real_publications, similarity_threshold=0.75)
 
-    def test_no_title(self):
-        real_publications = pd.DataFrame({})
-        threshold = 0.75
-        with self.assertRaises(KeyError):
-            FuzzyMatcher(real_publications, threshold)
+    return fuzzy_matcher
 
-    def test_no_string_titles(self):
-        real_publications = pd.DataFrame({
-            'title': [1,2]
-        })
-        threshold = 0.75
-        with self.assertRaises(AttributeError):
-            FuzzyMatcher(real_publications, threshold)
+def test_empty_list():
+    real_publications = []
+    with pytest.raises(ValueError):
+        FuzzyMatcher(real_publications, similarity_threshold=0.75)
 
-    def test_init_variables(self):
-        real_publications = pd.DataFrame({
-            'title': ['Malaria', 'Zika'],
-            'pmcid': [0, 1]
-        })
-        threshold = 0
-        fuzzy_matcher = FuzzyMatcher(real_publications, threshold)
-        assert_frame_equal(
-            fuzzy_matcher.publications, real_publications
-        )
-        self.assertEqual(
-            fuzzy_matcher.similarity_threshold, threshold
-        )
-        self.assertTrue(
-            fuzzy_matcher.tfidf_matrix.size != 0
-        )
+def test_no_titles():
+    real_publications = [{}, {}, {}]
+    with pytest.raises(ValueError):
+        FuzzyMatcher(real_publications, similarity_threshold=0.75)
 
-    def test_init_variables_jsonl(self):
-        real_publications = pd.DataFrame(
-            {'title': 'Malaria', 'pmcid': 0},
-            {'title': 'Zika', 'pmcid': 1},
-        )
-        threshold = 0
-        fuzzy_matcher = FuzzyMatcher(real_publications, threshold)
-        assert_frame_equal(
-            fuzzy_matcher.publications, real_publications
-        )
-        self.assertEqual(
-            fuzzy_matcher.similarity_threshold, threshold
-        )
-        self.assertTrue(
-            fuzzy_matcher.tfidf_matrix.size != 0
-        )
+def test_no_string_titles():
+    real_publications = [{'title':1}, {'title':2}]
+    with pytest.raises(AttributeError):
+        FuzzyMatcher(real_publications, similarity_threshold=0.75)
 
-class TestFuzzyMatch(unittest.TestCase):
-    def init_fuzzy_matcher(self):
-        real_publications = pd.DataFrame({
-            'title': ['Malaria', 'Zika'],
-            'uber_id': [1, 2]
-        })
-        threshold = settings.FUZZYMATCH_SIMILARITY_THRESHOLD
-        fuzzy_matcher = FuzzyMatcher(real_publications, threshold)
+def test_init_variables():
+    real_publications = [
+        {'title': 'Malaria', 'pmcid': 0},
+        {'title': 'Zika', 'pmcid': 1},
+    ]
 
-        return fuzzy_matcher
+    threshold = 0.75
+    fuzzy_matcher = FuzzyMatcher(real_publications, threshold)
+    assert isinstance(fuzzy_matcher.publications, dict)
+    assert isinstance(fuzzy_matcher.publications[0], dict)
+    assert fuzzy_matcher.publications[0] == real_publications[0]
+    assert fuzzy_matcher.similarity_threshold == threshold
+    assert fuzzy_matcher.tfidf_matrix.size != 0
 
-    def test_empty_reference(self):
-        fuzzy_matcher = self.init_fuzzy_matcher()
-        reference = {}
-        self.assertTrue(
-            fuzzy_matcher.match(reference) is None
-        )
 
-    def test_no_match(self):
-        fuzzy_matcher = self.init_fuzzy_matcher()
-        reference = {
-            'Document id': 1,
-            'Reference id': 1,
-            'Title': 'Ebola'
-        }
-        self.assertTrue(
-            fuzzy_matcher.match(reference) is None
-        )
 
-    def test_one_match(self):
-        fuzzy_matcher = self.init_fuzzy_matcher()
-        reference = {
-            'Document id': '10',
-            'Reference id': '11',
-            'Title': 'Malaria'
-        }
-        matched_publication = fuzzy_matcher.match(reference)
-        self.assertEqual(matched_publication['Document id'], '10')
+def test_empty_reference(fuzzy_matcher):
+    reference = {}
+    assert fuzzy_matcher.match(reference) is None
 
-    def test_close_match(self):
-        real_publications = pd.DataFrame({
-            'title': ['Malaria is caused by mosquitoes'],
-            'uber_id': [1]
-        })
-        threshold = settings.FUZZYMATCH_SIMILARITY_THRESHOLD
-        fuzzy_matcher = FuzzyMatcher(real_publications, threshold)
-        reference = {
-            'Document id':  '10',
-            'Reference id': '11',
-            'Title': 'Malaria'
-        }
-        matched_publication = fuzzy_matcher.match(reference)
-        self.assertEqual(matched_publication, None)
+def test_no_match(fuzzy_matcher):
+    reference = {
+        'Document id': 1,
+        'Reference id': 1,
+        'Title': 'Ebola'
+    }
+    assert fuzzy_matcher.match(reference) is None
+
+def test_one_match(fuzzy_matcher):
+    reference = {
+        'Document id': '10',
+        'Reference id': '11',
+        'Title': 'Malaria'
+    }
+    matched_publication = fuzzy_matcher.match(reference)
+    assert matched_publication['Document id'] == '10'
+
+def test_close_match():
+    real_publications = [
+        {'title': 'Malaria is caused by mosquitos', 'pmcid': 0},
+    ]
+    fuzzy_matcher = FuzzyMatcher(real_publications, similarity_threshold=0.75)
+
+    reference = {
+        'Document id':  '10',
+        'Reference id': '11',
+        'Title': 'Malaria',
+    }
+
+    matched_publication = fuzzy_matcher.match(reference)
+    assert matched_publication is None
+
+@pytest.mark.xfail(strict=True)
+def test_close_match_reverse(fuzzy_matcher):
+    """
+    This test is included to highlight somewhat unexpected behaviour, where
+    matches do not work in both directions. Compare to test above.
+    """
+
+    reference = {
+        'Document id':  '10',
+        'Reference id': '11',
+        'Title': 'Malaria is caused by mosquitos',
+    }
+
+    matched_publication = fuzzy_matcher.match(reference)
+    assert matched_publication is None
