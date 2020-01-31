f8fd2c22738a59162f9462d1ca814af4490f55a6
SamDepardieu <sam.depardieu@gmail.com>
2020-01-31 12:04:16 +0000

    Normalize titles case

--- reach/web/src/js/app.js
+++ reach/web/src/js/app.js
@@ -3,6 +3,11 @@ import policyTable from './policyTable.js';
 import citationsTable from './citationsTable.js';
 
 document.addEventListener('DOMContentLoaded', function(event) {
+    String.prototype.toTitleCase = function() {
+        let lower = this.valueOf().toLowerCase();
+        return lower.replace(/^\w/, c => c.toUpperCase());;
+    };
+
     clearSearch();
     policyTable();
     citationsTable();
