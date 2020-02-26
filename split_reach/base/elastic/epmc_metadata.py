1a0d4067ee13a2cbdb269c8066c58e7a404454d1
SamDepardieu <sam.depardieu@gmail.com>
2020-02-26 18:09:10 +0000

    Adds Elastic tools to the base image

---
+++ split_reach/base/elastic/epmc_metadata.py
@@ -0,0 +1,57 @@
+"""
+Inserts EPMC metadata into Elasticsearch.
+
+Sample URL for testing:
+
+    s3://datalabs-staging/airflow/output/open-research/epmc-metadata/epmc-metadata.json.gz
+"""
+
+import json
+import logging
+import functools
+
+from . import common
+
+CHUNK_SIZE = 1000  # tuned for small(ish) size of pub metadata
+
+
+def to_es_action(es_index, line):
+    d = json.loads(line)
+    return {
+        "_index": es_index,
+        "doc": d,
+    }
+
+
+def clean_es(es, es_index):
+    """ Ensure an empty index exists. """
+    common.recreate_index(es, es_index)
+
+
+def insert_file(f, es, es_index, max_items=None):
+    """
+    Inserts EPMC metadata from a json.gz file into Elasticsearch.
+
+    Args:
+        f: json.gz file object
+        es: a living connection to elacticsearch
+        max_items: maximum number of records to insert, or None
+    """
+    logging.info(
+        'epmc_metadata.insert_file: f=%s es=%s max_items=%s',
+        f, es, max_items)
+    to_es_func = functools.partial(to_es_action, es_index)
+    return common.insert_actions(
+        es,
+        common.yield_actions(f, to_es_func, max_items),
+        CHUNK_SIZE,
+        )
+
+
+if __name__ == '__main__':
+    def insert_func(f, es, max_items=None):
+        return insert_file(f, es, 'policy-test-epmc-metadata',
+                           max_items=max_items)
+    count = common.insert_from_argv(
+        __doc__.strip(), clean_es, insert_file)
+    logging.info('Imported %d pubs into ES', count)
---
+++ split_reach/base/elastic/import_sections_from_s3.py
@@ -0,0 +1,118 @@
+""" Takes a given file size (default to 100MB) from a dataset in S3 and
+import them into a running Elasticsearch database.
+"""
+
+import tempfile
+import csv
+import json
+import random
+
+import boto3
+from urllib.parse import urlparse
+from argparse import ArgumentParser
+from elasticsearch import Elasticsearch
+
+from . import common
+
+parser = ArgumentParser()
+parser.add_argument('file_url')
+
+parser.add_argument('-s', '--size',
+                    default=1024,
+                    type=int,
+                    help=('The megabytes to pull. Defaults to 100.'
+                          'A negative value will pull the entire dataset'))
+
+parser.add_argument('-H', '--host',
+                    default='127.0.0.1',
+                    help='Address of the Elasticsearch server')
+
+parser.add_argument('-P', '--port',
+                    default='9200',
+                    help='Port of the Elasticsearch server')
+
+parser.add_argument('-C', '--clean', dest='clean', action='store_true',
+                    help='Clean the elasticsearch database before import')
+
+
+def write_to_es(es, item):
+    """ Writes the given csv line to elasticsearch.
+
+    Args:
+        es: a living connection to elacticsearch
+        item: an dict from a json line.
+    """
+    if item["sections"]:
+        print(item['hash'])
+
+        section = item['sections']['Reference']
+        body = json.dumps({
+            'section': section,
+            'uri': item['uri'],
+            'hash': item['hash']
+        })
+        es.index(
+            index='datalabs-sections',
+            ignore=400,
+            body=body,
+            doc_type='section'
+        )
+
+
+def clean_es(es):
+    """ Empty the elasticsearch database.
+
+    Args:
+        es: a living connection to elasticsearch
+
+    """
+    common.recreate_index(es, 'datalabs-sections')
+
+
+def import_data(file_url, es, size):
+    """ Import data from a given file in elasticsearch.
+
+    Args:
+        file_url: a file url
+        es: a living connection to elasticsearch
+        size: the size of the data to pull in bytes
+
+    """
+    def yield_from_s3(s3_url, size):
+        s3 = boto3.resource('s3')
+        parsed_url = urlparse(s3_url)
+        print('Getting %s from %s bucket' % (parsed_url.path, parsed_url.netloc))
+        s3_file = s3.Object(bucket_name=parsed_url.netloc, key=parsed_url.path[1:])
+
+        with tempfile.TemporaryFile(mode='r+b') as tf:
+            rows = s3_file.get(Range='bytes=0-%d' % size)
+            for data in rows['Body']:
+                tf.write(data)
+
+            print('Got the fileobj')
+            tf.seek(0)
+            with open(tf.fileno(), mode='r', closefd=False) as json_file:
+                for line in json_file:
+                    yield line
+
+    def yield_from_local(file_url, size):
+        with open(file_url) as f:
+            for line in f:
+                yield line
+
+    yield_lines = yield_from_s3 if file_url.startswith('s3://') else yield_from_local
+    for line in yield_lines(file_url, size):
+        item = json.loads(line)
+        write_to_es(es, item)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    es = Elasticsearch([{'host': args.host, 'port': args.port}])
+
+    if args.clean:
+        clean_es(es)
+
+    size = args.size * 1024 * 1000 ** 2
+    import_data(args.file_url, es, size)
