c623c5b2f378714bb7cf1e8cdaa5e470a8e32512
Elizabeth Gallagher <lizgzil@hotmail.com>
2020-05-04 12:32:32 +0100

    Delete test of deep splitparser and associated config file

--- split_reach/extracter/refparse/tests/test_split_parse.py
+++ split_reach/extracter/refparse/tests/test_split_parse.py
@@ -1,54 +1,7 @@
-import os.path
 import unittest
-import pickle
-
 import pytest
 
 from refparse.utils import structure_reference
-from refparse.refparse import SectionedDocument
-from deep_reference_parser.split_parse import SplitParser
-
-splitter_parser = SplitParser(config_file=os.path.join(
-    os.path.dirname(__file__), 'test_config_multitask.ini')
-)
-
-class TestSplitParse(unittest.TestCase):
-
-    def test_empty_sections(self):
-        references = splitter_parser.split_parse(" ")
-        assert references == [], "Should be []"
-
-    @pytest.mark.xfail()
-    def test_oneline_section(self):
-        references = splitter_parser.split_parse("References. Smith et al. 2019. This is a title. Journal of journals. 1-2")
-        assert len(references) == 1, "There should be 1 reference found"
-
-    def test_oneline_section_brackets(self):
-        references = splitter_parser.split_parse("References. Smith et al. (2019). This is a title. Journal of journals. 1-2")
-        assert len(references) == 1, "There should be 1 reference found"
-
-    def test_empty_lines_section(self):
-        references = splitter_parser.split_parse("\n\n\n")
-        assert references == [], "Should be []"
-
-    @pytest.mark.xfail()
-    def test_normal_section(self):
-        references = splitter_parser.split_parse(
-            "References \n1. Smith et al. 2019. This is a title. Journal of journals. 1-2. "+
-            "\n2. Smith et al. 2019. This is a title. Journal of journals. 1-2. "+
-            "\n3. Smith et al. 2019. This is a title. Journal of journals. 1-2."
-        )
-
-        assert len(references) == 3, "There should be 3 reference found"
-
-    def test_normal_section_brackets(self):
-        references = splitter_parser.split_parse(
-            "References \n1. Smith et al. (2019). This is a title. Journal of journals. 1-2. "+
-            "\n2. Smith et al. (2019). This is a title. Journal of journals. 1-2. "+
-            "\n3. Smith et al. (2019). This is a title. Journal of journals. 1-2."
-        )
-
-        assert len(references) == 3, "There should be 3 reference found"
 
 class TestStructure(unittest.TestCase):
     def test_empty_components(self):
