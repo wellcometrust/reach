eeee9141204744694a7cb015b917ea2e1e553741
Samuel Depardieu <6536364+SamDepardieu@users.noreply.github.com>
2020-05-14 10:23:15 +0200

    Build Reach static files on codebuild (#514)
    * Adds the build folder to the repository
    
    * Remove unused docker/git ignore
    
    * Adds a recipe to build reach web app's static files
    
    * Adds the docs build folder

--- web/bin/docker_run.sh
+++ web/bin/docker_run.sh
@@ -2,8 +2,8 @@
 # Wrapper for running commands inside the web build image.
 # Mirrors what's in ../docker-compose.yml.
 
-DIR=$(cd $(dirname $0)/..; pwd)
-BUILD_DIR=$(cd $(dirname $0)/../../../build/web/static; pwd)
+DIR=$(cd $(dirname $0)/../app/; pwd)
+BUILD_DIR=$(cd $(dirname $0)/../build/web/static; pwd)
 
 # NODE_ENV=production silences an erroneous warning about postcss
 # not being configured that I couldn't silence otherwise. :-(
@@ -11,7 +11,7 @@ docker run \
     --rm \
     -e NODE_ENV=production \
     -v $BUILD_DIR/:/opt/reach/build/web/static \
-    -v $DIR/gulpfile.js:/opt/reach/build/web/gulpfile.js \
+    -v $DIR/../gulpfile.js:/opt/reach/build/web/gulpfile.js \
     -v $DIR/src:/opt/reach/build/web/src \
     reach-web-build:latest \
     $*
