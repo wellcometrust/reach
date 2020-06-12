e87e8af819a015d636c5784d2072dc88ed996728
Jeff Uren <jduren@protonmail.com>
2020-06-12 14:05:31 +0100

    - Fixes table sorting - Publications with less than 1 citations are now excluded from search results

--- web/web/views/api/api_search_citations.py
+++ web/web/views/api/api_search_citations.py
@@ -1,5 +1,7 @@
 import json
 
+from psycopg2.extensions import AsIs
+
 from web.db import get_db_cur
 from .utils import JSONEncoder
 
@@ -9,35 +11,49 @@ from .utils import JSONEncoder
 # specific query
 SQL = """
 WITH results AS (
-    SELECT *,
-            ts_rank_cd(epmc.tsv_omni, query) AS rank
-    FROM warehouse.epmc_metadata AS epmc,
-        websearch_to_tsquery(%s) AS query
-    WHERE query @@ epmc.tsv_omni
-    ORDER BY rank DESC
-    LIMIT 25
+    SELECT epmc.*,
+           rc.policies AS source_policies,
+           rc.uuid,
+           ts_rank_cd(epmc.tsv_omni, query) as rank
+    FROM warehouse.epmc_metadata AS epmc
+    LEFT JOIN warehouse.reach_citations AS rc ON rc.epmc_id = epmc.uuid
+    CROSS JOIN websearch_to_tsquery(%s) AS query
+    WHERE array_length(rc.policies, 1) > 0
+        AND query @@ epmc.tsv_omni
+    ORDER BY %s %s
+    LIMIT %s
     OFFSET %s
 ) SELECT r.*,
-    (SELECT json_agg(items)
-        FROM (
-                    SELECT title, sub_title, year, source_doc_url, source_org, scrape_source_page FROM warehouse.reach_policies AS cp
-            WHERE c.policies @> ARRAY[cp.uuid]::UUID[]
-
-            ) AS items
-        ) AS policies
-FROM results AS r
-LEFT JOIN warehouse.reach_citations as c ON c.epmc_id = r.uuid;
+         (SELECT json_agg(items)
+             FROM (
+                  SELECT title, sub_title, year, source_doc_url, source_org, scrape_source_page FROM warehouse.reach_policies AS cp
+                 WHERE r.source_policies @> ARRAY[cp.uuid]::UUID[]
+                      ) AS items
+             ) AS policies
+FROM results AS r;
 """
 
 SQL_COUNT = """
 WITH results AS (
-    SELECT uuid,
+    SELECT epmc.uuid,
             ts_rank_cd(epmc.tsv_omni, query) AS rank
-    FROM warehouse.epmc_metadata AS epmc,
-        websearch_to_tsquery(%s) AS query
+    FROM warehouse.epmc_metadata AS epmc
+        LEFT JOIN warehouse.reach_citations AS rc ON rc.epmc_id = epmc.uuid
+        CROSS JOIN websearch_to_tsquery(%s) AS query
     WHERE query @@ epmc.tsv_omni
+        AND array_length(rc.policies, 1) > 0
 ) SELECT COUNT(uuid) AS counter FROM results;
 """
+
+DEFAULT_ORDER = ("rank", "DESC",)
+ALLOWABLE_ORDERS = (
+    "match_publication",
+    "match_title.keyword",
+    "match_pub_year",
+    "associated_policies_count",
+)
+
+
 
 class ApiSearchCitations:
 
@@ -55,19 +71,56 @@ class ApiSearchCitations:
         # TODO: Need to rate-limit this for external hosts
 
         if req.params:
+            sort = req.params.get("sort", None)
+            order = req.params.get("order", "DESC")
             terms = req.params.get("terms", None)
             limit = req.params.get("size", 25)
             page = int(req.params.get("page", 1))
 
+            if order.upper() not in (None, "DESC", "ASC",):
+                resp.content_type = "application/json"
+                resp.body = json.dumps({
+                    'status': 'error',
+                    'message': "Invalid ordering operation"
+                })
+                return
+
+            if sort not in ALLOWABLE_ORDERS:
+                resp.content_type = "application/json"
+                resp.body = json.dumps({
+                    'status': 'error',
+                    'message': "Invalid ordering operation"
+                })
+                return
+
             offset = 0
             if page > 1:
-                offset = page * 25
+                offset = page * int(limit)
 
             counter = 0
             results = []
+
+            # TODO: These terms need to be changed on the frontend
+            if sort is None:
+                orders = DEFAULT_ORDER
+            else:
+                if sort == "match_publication":
+                    orders = ("epmc.journal_title", order)
+                elif sort == "match_title.keyword":
+                    orders = ("epmc.title", order)
+                elif sort == "match_pub_year":
+                    orders = ("epmc.pub_year", order)
+                elif sort == "associated_policies_count":
+                    orders = ("array_length(rc.policies, 1)", order)
+                else:
+                    orders = DEFAULT_ORDER
+
             with get_db_cur() as cur:
                 cur.execute(SQL, (
                     terms,
+                    AsIs(orders[0]),
+                    AsIs(orders[1]),
+                    limit,
                     offset,
                 ))
                 results = cur.fetchall()
